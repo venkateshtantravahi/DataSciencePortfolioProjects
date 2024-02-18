205203a7281485d858befaae81a5d3cad268a8a5
Venkatesh Tantravahi <venkateshtantravahi99@gmail.com>
2024-02-18 13:41:11 -0500

    added some more utility frunctions to support plotting

--- utils/prophet_utils.py
+++ utils/prophet_utils.py
@@ -1,5 +1,7 @@
 import pandas as pd
 from typing import List
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 
 def clean_dataframe(df, date_column: str, unwanted_columns: List):
@@ -50,3 +52,48 @@ def remove_duplicates(df, subset):
     - pd.DataFrame: The dataframe with duplicates removed.
     """
     return df.drop_duplicates(subset=subset)
+
+
+def plot_cumulative_trends(df, date_col, data_cols, title):
+    """
+    Plots the cumulative trends of confirmed, deaths, and recovered cases.
+
+    Parameters:
+    - df (pd.DataFrame): The input dataframe.
+    - date_col (str): The name of the column containing date information.
+    - data_cols (list): List of column names to be plotted.
+    - title (str): The title of the plot.
+
+    Returns:
+    - None
+    """
+    plt.figure(figsize=(14, 7))
+    for data_col in data_cols:
+        sns.lineplot(x=date_col, y=data_col, data=df, label=data_col)
+    plt.title(title)
+    plt.xlabel('Date')
+    plt.ylabel('Count')
+    plt.legend()
+    plt.show()
+
+def plot_daily_changes(df, date_col, data_cols, title):
+    """
+    Plots the daily changes in confirmed, deaths, and recovered cases.
+
+    Parameters:
+    - df (pd.DataFrame): The input dataframe.
+    - date_col (str): The name of the column containing date information.
+    - data_cols (list): List of column names to be plotted.
+    - title (str): The title of the plot.
+
+    Returns:
+    - None
+    """
+    plt.figure(figsize=(14, 7))
+    for data_col in data_cols:
+        sns.lineplot(x=date_col, y=df[data_col].diff(), data=df, label=f'Daily {data_col}')
+    plt.title(title)
+    plt.xlabel('Date')
+    plt.ylabel('Daily Change')
+    plt.legend()
+    plt.show()
